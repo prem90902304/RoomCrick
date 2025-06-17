8c15dd38f74ceb3aacedeb377ef3bebcca4e5f45
MUPPALAM PREMSAI <com056032@gmail.com>
2025-06-17 16:33:27 +0530

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,18 @@
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open("crickscore-cache").then(cache =>
+      cache.addAll([
+        "./",
+        "./index.html",
+        "./manifest.json",
+        "./service-worker.js"
+      ])
+    )
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(response => response || fetch(e.request))
+  );
+});
